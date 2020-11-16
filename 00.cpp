2bf53393066ccec784168683573b12a8ea0d4758
kumail27 <74414291+kumail27@users.noreply.github.com>
2020-11-16 17:14:02 +0500

    a<b
    if and else commands

---
+++ 00.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+
+	int a, b;
+		printf("the first value is");
+	scanf("%d",& a);
+	printf("the second value is");
+	scanf("%d",& b);
+
+	if (a<b)
+	{ printf("a<b");
+	}
+	else	
+	{		
+	printf("a>b");
+
+}
+}
